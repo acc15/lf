bedad993d7e233a67748cf26d30846a6184d2ebf
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2022-12-15 03:12:41 +0300

    sync

---
+++ src/cmd/cmd_desc.hpp
@@ -0,0 +1,23 @@
+#include <span>
+#include <fmt/format.h>
+
+namespace leafsync
+{
+
+struct cmd_desc
+{
+    std::span<const char *> names;
+    const char *description;
+};
+
+}
+
+template <>
+struct fmt::formatter<leafsync::cmd_desc> : fmt::formatter<string_view>
+{
+    template <typename FormatContext>
+    auto format(const leafsync::cmd_desc &c, FormatContext &ctx) const
+    {
+        return fmt::format_to(ctx.out(), "{} - {}", fmt::join(c.names, ", "), c.description);
+    }
+};
