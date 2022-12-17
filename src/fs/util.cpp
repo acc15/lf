a044acc762a601892784879bb902be3b80a46f27
acc15 <vyacheslav.mayorov@gmail.com>
2022-12-17 22:30:34 +0300

    MSVC build

--- src/fs/util.cpp
+++ src/fs/util.cpp
@@ -3,7 +3,13 @@
 namespace leafsync {
 
 std::chrono::system_clock::time_point ft_to_sys(const std::chrono::file_clock::time_point& v) {
+// TODO: std::chrono::clock_cast not implemented in GNU C++ library (however it provides file_clock::to_sys)
+// After implementation of clock_cast in GNU lib - this #ifdef should be replaced with clock_cast
+#ifdef __GNUG__
     return std::chrono::file_clock::to_sys(v);
+#else
+    return std::chrono::clock_cast<std::chrono::system_clock>(v);
+#endif
 }
 
 }
