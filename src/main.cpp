897e9355fbe3cb17af2bf35935a47f74ff3f11d9
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2022-12-20 02:35:28 +0300

    setting console utf-8 for windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include "cmd/cmd_registry.hpp"
 
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
 int main(const int argc, const char *argv[]) {
+
+#ifdef WIN32
+	SetConsoleOutputCP(CP_UTF8);
+#endif
+    
     std::span<const char*> args(argv, argc);
     return leafsync::cmds.run(args.subspan(1));
+    
 }
