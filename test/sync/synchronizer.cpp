b5feb94d01321b5f2ae27a687c72bc049f452b8b
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2023-01-08 08:20:56 +0300

    sync tests

--- test/sync/synchronizer.cpp
+++ test/sync/synchronizer.cpp
@@ -1,15 +1,21 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 
 #include <thread>
 
+#include "io/format_stream.hpp"
 #include "sync/synchronizer.hpp"
 #include "test_util.hpp"
 
 namespace fs = std::filesystem;
 using namespace lf;
 
-config::sync make_sync() {
+config::sync make_sync(const std::string& suffix = "") {
     std::string name = Catch::getResultCapture().getCurrentTestName();
+    if (!suffix.empty()) {
+        name += " " + suffix;
+    }
+
     fs::path dest_test_dir = fs::temp_directory_path() / "lf_test" / "synchronizer" / name;
 
     fs::remove_all(dest_test_dir);
@@ -41,7 +47,7 @@ fs::file_time_type plus50ms_last_write_time(const std::filesystem::path& dst, co
 const std::string test_name = "test.txt";
 const std::string test_content = "abc\nxyz";
 
-TEST_CASE("local added", "[synchronizer]") {
+TEST_CASE("local file added", "[synchronizer]") {
     // must copy to remote and set state
     auto sync = make_sync();
 
@@ -58,9 +64,10 @@ TEST_CASE("local added", "[synchronizer]") {
     REQUIRE( read_text(sync.remote / test_deep_path ) == test_content );
     REQUIRE( fs::last_write_time(sync.remote / test_deep_path) == fs::last_write_time(sync.local / test_deep_path) );
     REQUIRE( fs::exists(sync.local / test_deep_path) );
+
 }
 
-TEST_CASE("local deleted", "[synchronizer]") {
+TEST_CASE("local file deleted", "[synchronizer]") {
     // must remove file from remote, remove it from index and reset sync
     auto sync = make_sync();
 
@@ -79,19 +86,21 @@ TEST_CASE("local deleted", "[synchronizer]") {
     REQUIRE_FALSE( fs::exists(sync.local  / test_name) );
 }
 
-TEST_CASE("remote deleted", "[synchronizer]") {
+TEST_CASE("remote file deleted", "[synchronizer]") {
     // must remove file from local and reset state
     auto sync = make_sync();
 
     write_text(sync.local / test_name, test_content);
     
     synchronizer s("test", sync);
-    s.index = index_tree();
+    // index is empty due to file already removed from index (see test above)
     s.state = state_tree(false, { {test_name, state_tree(true) } });
     s.run();
 
     REQUIRE_FALSE( fs::exists(sync.remote / test_name) );
     REQUIRE_FALSE( fs::exists(sync.local / test_name) );
+    REQUIRE_FALSE( s.state.get(test_name) );
+
 }
 
 TEST_CASE("remote added", "[synchronizer]") {
@@ -123,81 +132,80 @@ TEST_CASE("index removed", "[synchronizer]") {
 }
 
 TEST_CASE("can update file timestamp", "[synchronizer]") {
-    auto sync = make_sync();
-    write_text(sync.local / test_name, "abc\n");
-    write_text(sync.remote / test_name, "abc\n");
-    plus50ms_last_write_time(sync.local / test_name, sync.remote / test_name);
-}
-
-TEST_CASE("update local from remote", "[synchronizer]") {
-
-    auto sync = make_sync();
-
-    write_text(sync.local / test_name, "abc\n");
-    write_text(sync.remote / test_name, test_content);
-
-    auto change_t = plus50ms_last_write_time(sync.local / test_name, sync.remote / test_name);
-
-    synchronizer s("test", sync);
-    s.index = index_tree(sync_mode::UNSPECIFIED, { {test_name, index_tree(sync_mode::SHALLOW) } });
-    s.run();
-
-    REQUIRE( read_text(sync.remote / test_name) == test_content );
-    REQUIRE( read_text(sync.local / test_name) == test_content );
-    REQUIRE( fs::last_write_time(sync.remote / test_name) == change_t );
-    REQUIRE( fs::last_write_time(sync.local / test_name) == change_t );
-    REQUIRE( s.state.get(test_name) == true );
-
-}
-
-TEST_CASE("update remote from local", "[synchronizer]") {
-
-    auto sync = make_sync();
-
-    write_text(sync.local / test_name, test_content);
-    write_text(sync.remote / test_name, "abc\n");
-
-    auto change_t = plus50ms_last_write_time(sync.local / test_name, sync.remote / test_name);
-
-    synchronizer s("test", sync);
-    s.index = index_tree(sync_mode::UNSPECIFIED, { {test_name, index_tree(sync_mode::SHALLOW) } });
-    s.run();
-
-    REQUIRE( read_text(sync.local / test_name) == test_content );
-    REQUIRE( read_text(sync.remote / test_name) == test_content );
-    REQUIRE( fs::last_write_time(sync.local / test_name) == change_t );
-    REQUIRE( fs::last_write_time(sync.remote / test_name) == change_t );
-    REQUIRE( s.state.get(test_name) == true );
-
-}
-
-TEST_CASE("local file overwrites remote dir", "[synchronizer]") {
     
     auto sync = make_sync();
-    write_text(sync.local / test_name, test_content);
-    write_text(sync.remote / test_name / "x.txt", "xyz");
-
+    write_text(sync.local / test_name, "abc\n");
+    write_text(sync.remote / test_name, "abc\n");
     plus50ms_last_write_time(sync.local / test_name, sync.remote / test_name);
+
+}
+
+TEST_CASE("update file", "[synchronizer]") {
+
+    bool local_or_remote = GENERATE(true, false);
+
+    std::string name = local_or_remote ? "local to remote" : "remote to local";
+    INFO(name);
+
+    auto sync = make_sync(" " + name);
+
+    fs::path l = local_or_remote ? sync.local : sync.remote;
+    fs::path r = !local_or_remote ? sync.local : sync.remote;
+
+    write_text(l / test_name, "abc\n");
+    write_text(r / test_name, test_content);
+
+    auto change_t = plus50ms_last_write_time(l / test_name, r / test_name);
 
     synchronizer s("test", sync);
     s.index = index_tree(sync_mode::UNSPECIFIED, { {test_name, index_tree(sync_mode::SHALLOW) } });
     s.run();
 
-    REQUIRE( fs::status(sync.remote / test_name).type() == fs::file_type::regular );
-    REQUIRE( read_text(sync.remote / test_name) == test_content );
+    REQUIRE( read_text(r / test_name) == test_content );
+    REQUIRE( read_text(l / test_name) == test_content );
+    REQUIRE( fs::last_write_time(r / test_name) == change_t );
+    REQUIRE( fs::last_write_time(l / test_name) == change_t );
+    REQUIRE( s.state.get(test_name) == true );
 
 }
 
-TEST_CASE("local dir overwrites remote file", "[synchronizer]") {
+TEST_CASE("type ovewrite", "[synchronizer]") {
+    
+    bool local_or_remote = GENERATE(true, false);
+    bool file_or_dir = GENERATE(true, false);
 
+    std::string name = lf::format_stream()
+        << (local_or_remote ? "local" : "remote") << " "
+        << (file_or_dir ? "file" : "dir") << " to "
+        << (!local_or_remote ? "local" : "remote") << " "
+        << (!file_or_dir ? "file" : "dir");
 
-}
+    INFO(" " + name);
 
-TEST_CASE("remote file overwrites local dir", "[synchronizer]") {
+    auto sync = make_sync(name);
 
-}
+    write_text((local_or_remote ^ file_or_dir ? sync.remote : sync.local) / test_name, test_content);
+    write_text((local_or_remote ^ file_or_dir ? sync.local : sync.remote) / test_name / "x.txt", test_content);
 
-TEST_CASE("remote dir overwrites local file", "[synchronizer]") {
+    plus50ms_last_write_time(
+        (local_or_remote ? sync.local : sync.remote) / test_name, 
+        (!local_or_remote ? sync.local : sync.remote) / test_name
+    );
+
+    synchronizer s("test", sync);
+    s.index = index_tree(sync_mode::UNSPECIFIED, { {test_name, index_tree(sync_mode::SHALLOW) } });
+    s.run();
+
+    fs::file_type expected_type = file_or_dir ? fs::file_type::regular : fs::file_type::directory;
+    fs::path target_path = (local_or_remote ? sync.remote : sync.local) / test_name;
+    REQUIRE( fs::status(target_path).type() == expected_type );
+    
+    fs::path check_path = target_path / test_name;
+    if (!file_or_dir) {
+        check_path /= "x.txt";
+    } 
+
+    REQUIRE( read_text(check_path) == test_content );
 
 }
 
