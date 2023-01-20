e3f995616ccd66e53fe6f6e9e9f65bebbf8dd848
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2023-01-20 17:56:48 +0300

    log_level_filter

---
+++ src/io/log/filter/log_level_filter.cpp
@@ -0,0 +1,12 @@
+#include "io/log/filter/log_level_filter.hpp"
+
+namespace lf {
+
+    log_level_filter::log_level_filter(log_level min_level): min_level(min_level) {
+    }
+
+    bool log_level_filter::level(log_level level) const {
+        return level >= min_level;
+    }
+
+}
---
+++ src/io/log/filter/log_level_filter.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "io/log/filter/log_filter.hpp"
+
+namespace lf {
+
+    class log_level_filter: public log_filter {
+        log_level min_level;
+
+    public:
+        log_level_filter(log_level min_level);
+        bool level(log_level level) const override;
+    };
+
+}
