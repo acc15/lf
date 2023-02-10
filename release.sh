40040c1d5130d1eae2fe63760215ca6cedb0d53d
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2023-02-10 11:26:39 +0300

    workflow preset

--- release.sh
+++ release.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-BUILD_DIR="build/release"
-
-rm -rf "$BUILD_DIR"
-cmake --preset default .
-cmake --build "$BUILD_DIR" --target lf --config Release
-cmake --install "$BUILD_DIR"
+rm -rf build
+cmake --workflow --preset release
+cmake --install build
