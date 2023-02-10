f2fddc15a0674a49531f6f50c40fc1cba46666a7
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2023-02-10 15:24:36 +0300

    fix script

--- release.bat
+++ release.bat
@@ -1,8 +1,5 @@
 @echo off
 
-set BUILD_DIR="build\release"
-
-rmdir "%BUILD_DIR%" /s /q
-cmake --preset default .
-cmake --build "%BUILD_DIR%" --target lf --config Release
-cmake --install "%BUILD_DIR%"
+rmdir "build" /s /q
+cmake --workflow --preset release
+cmake --install "build"
