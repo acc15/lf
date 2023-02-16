0a5a20d02aa9d2509207c7f9e467897c7489376b
acc15 <vyacheslav.mayorov@gmail.com>
2023-02-17 02:50:43 +0300

    windows FILE* access thru hack

--- src/fs/adv_fstream.hpp
+++ src/fs/adv_fstream.hpp
@@ -1,18 +1,24 @@
 #pragma once
 
 #include <iostream>
-#include <fstream>
 #include <stdio.h>
+
+#ifdef _WIN32
+#   define private protected // Very bad ugly hack but better than reimplement basic_filebuf
+#   include <fstream>
+#   undef private // End of very bad and ugly hack
+#   include <Windows.h>
+#else
+#   include <fstream>
+#endif 
 
 namespace lf {
 
     template <typename Elem, typename Traits>
     class basic_adv_filebuf: public std::basic_filebuf<Elem, Traits> {
-    public:
-        ~basic_adv_filebuf() noexcept override {}
 
 #if defined (__GLIBCXX__)
-
+    public:
         FILE* file() const {
             return const_cast<basic_file&>(_M_file).file();
         }
@@ -22,20 +28,30 @@ namespace lf {
         }
 
 #elif defined (_MSC_VER)
-
+        using std::basic_filebuf<Elem, Traits>::_Myfile;
+    public:
         FILE* file() const {
-            return _file;
+            return _Myfile;
         }
 
         int fd() const {
-            return _fileno(_file);
+            return _fileno(_Myfile);
         }
 
-    private:
-        FILE* _file;
+#ifdef _WIN32
+        HANDLE handle() const {
+            return _get_osfhandle(fd());
+        }
+#endif
+
 #else
 #   error "Unsupported platform!!!"
 #endif
+
+        basic_adv_filebuf() {}
+        basic_adv_filebuf(basic_adv_filebuf&& m): std::basic_filebuf<Elem, Traits>(std::move(m)) {}
+        ~basic_adv_filebuf() noexcept override {}
+
     };
 
     /**
@@ -44,7 +60,7 @@ namespace lf {
     - access to internal FILE* and file descriptor
     - ability to truncate file after creation
     **/
-    template <typename Char, typename Traits, typename Parent, std::ios_base::openmode DefaultMode, std::ios_base::openmode AddMode = 0>
+    /*template <typename Char, typename Traits, typename Parent, std::ios_base::openmode DefaultMode, std::ios_base::openmode AddMode = 0>
     class basic_adv_fstream: public Parent {
     public:
         basic_adv_fstream(const char* path, std::ios_base::openmode mode = DefaultMode) {
@@ -64,6 +80,6 @@ namespace lf {
 
     private:
 
-    };
+    };*/
 
 }
--- test/fs/lock.cpp
+++ test/fs/lock.cpp
@@ -1,4 +1,4 @@
-#include <fstream>
+#include "fs/adv_fstream.hpp"
 #include <filesystem>
 #include <iostream>
 #include <catch2/catch_test_macros.hpp>
