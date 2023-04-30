2064819240807113862bccc9b84f0692b0a41682
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2023-05-01 01:58:36 +0300

    joiner concepts

--- src/io/joiner.hpp
+++ src/io/joiner.hpp
@@ -1,38 +1,64 @@
 #include <ostream>
-#include <ranges>
 #include <functional>
 
 namespace lf {
 
-    template <typename Range, typename Mapper>
+    template <
+        std::input_iterator It, 
+        std::sentinel_for<It> EndIt,
+        std::invocable<decltype(*std::declval<It>())> Mapper
+    >
     struct joiner {
-        joiner(std::string_view separator, const Range& range, Mapper mapper): separator(separator), range(range), mapper(mapper) {}
+        joiner(std::string_view separator, const It& begin, const EndIt& end, Mapper mapper): 
+            separator(separator), 
+            begin(begin),
+            end(end),
+            mapper(mapper) 
+        {
+        }
+
         std::string_view separator;
-        const Range& range; 
+        const It begin; 
+        const EndIt end; 
         const Mapper mapper;
     };
 
     template <typename Range>
-    joiner<Range, std::identity> join(std::string_view separator, const Range& range) {
-        return joiner<Range, std::identity>(separator, range, {});
+    auto join(std::string_view separator, const Range& range) {
+        return joiner(separator, std::begin(range), std::end(range), std::identity {});
     }
 
-    template <typename Range, typename Mapper>
-    joiner<Range, Mapper> join(std::string_view separator, const Range& range, Mapper mapper) {
-        return joiner<Range, Mapper>(separator, range, mapper);
+    template <
+        typename Range, 
+        std::invocable<decltype(*std::begin(std::declval<Range>()))> Mapper
+    >
+    auto join(std::string_view separator, const Range& range, Mapper mapper) {
+        return joiner(separator, std::begin(range), std::end(range), mapper);
+    } 
+
+    template <
+        std::input_iterator It, 
+        std::sentinel_for<It> EndIt, 
+        std::invocable<decltype(*std::declval<It>())> Mapper
+    >
+    auto join(std::string_view separator, It begin, EndIt end, Mapper mapper) {
+        return joiner(separator, begin, end, mapper);
     }
 
-    template <typename Range, typename Mapper>
-    std::ostream& operator<<(std::ostream& s, const joiner<Range, Mapper>& joiner) {
-        auto it = std::begin(joiner.range);
-        const auto end = std::end(joiner.range);
-        if (it == end) {
+    template <
+        std::input_iterator It, 
+        std::sentinel_for<It> EndIt, 
+        std::invocable<decltype(*std::declval<It>())> Mapper
+    >
+    std::ostream& operator<<(std::ostream& s, const joiner<It, EndIt, Mapper>& joiner) {
+        auto it = joiner.begin;
+        if (it == joiner.end) {
             return s;
         }
         while (true) {
             s << joiner.mapper(*it);
             ++it;
-            if (it == end) {
+            if (it == joiner.end) {
                 break;
             }
             s << joiner.separator;
@@ -40,4 +66,4 @@ namespace lf {
         return s;
     }
 
-}
+} 
