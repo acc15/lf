160477a83a0b8639291f6d995c469fd047e28657
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2023-06-23 02:08:10 +0300

    glob impl

--- src/glob/globdef.hpp
+++ src/glob/globdef.hpp
@@ -43,12 +43,52 @@ namespace lf {
     private:
         matcher_vector matchers;
 
+        struct repetition_info {
+            pos_type position;
+            size_t index;
+            size_t repetition;
+        };
+
     public:
         glob(matcher_vector&& matchers = {}): matchers(std::move(matchers)) {}
 
         bool matches(streambuf* buf) const {
-            std::vector<std::pair<pos_type, size_t>> repetition_stack;
+            std::vector<repetition_info> rep;
 
+            size_t i = 0;
+            while (i < matchers.size()) {
+                const matcher& m = *matchers[i];
+
+                size_t repetition = 0;
+                if (m.is_repetitive()) {
+                    if (!rep.empty() && rep.back().index == i) {
+                        repetition = rep.back().repetition;
+                    } else {
+                        pos_type pos = buf->pubseekoff(0, std::ios_base::cur, std::ios_base::in);
+                        rep.push_back(repetition_info { pos, i, repetition });
+                    }
+                }
+                
+                const bool is_last = i == (matchers.size() - 1);
+                if (m.matches(buf, repetition, is_last)) {
+                    ++i;
+                    continue;
+                }
+
+                if (rep.empty()) {
+                    return false;
+                }
+
+                if (m.is_repetitive()) {
+                    rep.pop_back();
+                }
+
+                repetition_info& r = rep.back();
+                buf->pubseekpos(r.position, std::ios_base::in);
+                i = r.index;
+                ++r.repetition;
+
+            }
             return buf->sgetc() == char_traits::eof();
         }
 
